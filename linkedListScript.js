6de4aaff6c77bd988fec12022e9270cdb58961d2
de Lellis <delellis021@gmail.com>
2024-09-07 19:27:28 -0300

    Refatoração de lista ligada

--- linkedListScript.js
+++ linkedListScript.js
@@ -17,20 +17,32 @@ class Queue {
         this.start = null;
         this.end = null;
         this.length = 0;
-    }
+        this.idCounter = 1;
+    };
     // método para adição de usuários na fila
     // deve ser utilizado para adicionar um usuário na fila
+    // caso a prioridade seja 1, o usuário deve ser inserido entre dois usuário com prioridade 0
+    // to-do: testar possíveis erros
     addNextUser(name, cpf, priority){
-        const newUser = new User(name, cpf, this.length+1);
-        if (this.end != null) {
-            this.end.next = newUser;
-        }
-        this.end = newUser;
-        if (this.start == null){
+        const newUser = new User(name, cpf, this.idCounter, priority);
+        // criação da lista
+        if (this.start == null) {
             this.start = newUser;
+            this.end = newUser;
+        }
+        // adição caso possua prioridade
+        // to-do: mudar condicional
+        // to-do: escrever algoritmo de inserção
+        else if (priority != null){
+        }
+        // adição ao fim da lista
+        else {
+            this.end.next = newUser;
+            this.end = newUser;
         }
         this.length++;
-    };  
+        this.idCounter++;
+    };
     // método para remover o primeiro usuário da fila
     // deve ser utilizado quando uma consulta for concluída
     removeTopUser() {
@@ -43,6 +55,8 @@ class Queue {
             this.end = null;
         }
         this.length--;
+        patient.next = null;
+        return patient;
     };
     // método para exibir o usuário no topo da fila
     showTopUser() {
@@ -50,12 +64,8 @@ class Queue {
     };
     // método para trocar usuários no caso de espera
     // deve ser utilizado para trocar a posição de um usuário ausente
-    // obs.: quando o usuário é trocado com o último elemento da lista,
-    // o campo "end" continua sendo o elemento anterior, porém o campo "next"
-    // se torna o usuário a ser esperado.
-    // to-do: verificar se o comportamento descrito acima é um problema
-    // to-do: buscar por soluções caso o comportamento seja um problema
     swapUserWithTheNext(userId){
+        // não deve haver troca se o início da lista ou o elemento seguinte não existir
         if (this.start == null || this.start.next == null){
             return;
         }
@@ -98,13 +108,27 @@ class Queue {
         }
         this.swapUserWithTheNext(tmp.id);
     };
+    // método para testes
+    printQueue(){
+        let start = this.start;
+        while (start.next != null){
+            console.log(`Nome: ${start.name} | CPF: ${start.cpf} | ID: ${start.id} | Priority: ${start.priority}`);
+            start = start.next;
+        };
+        console.log(`Nome: ${start.name} | CPF: ${start.cpf} | ID: ${start.id} | Priority: ${start.priority}`);
+    }
 };
 // testes com a estrutura de dados criada acima
-teste = new Queue();
-teste.addNextUser('Camilo', '123');
-teste.addNextUser('de Lellis', '789');
-teste.addNextUser('Medeiros', '789');
-teste.addNextUser('Santos', '321');
-teste.addNextUser('Rosendo', '654');
-teste.waitForUser(2);
-console.log(teste.start.next);
+let teste = new Queue();
+teste.addNextUser('Camilo', '123', 0);
+teste.addNextUser('de Lellis', '789', 0);
+teste.addNextUser('Medeiros', '789', 1);
+teste.addNextUser('Santos', '321', 0);
+teste.addNextUser('Rosendo', '654', 1);
+//teste.waitForUser(2);
+teste.printQueue();
+/*
+let camilo = teste.removeTopUser();
+console.log(camilo);
+teste.printQueue();
+*/
