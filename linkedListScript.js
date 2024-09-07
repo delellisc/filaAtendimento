ee090bf655711677a68650555aaf0441ff3034ab
de Lellis <delellis021@gmail.com>
2024-09-07 08:20:16 -0300

    Testes na lista ligada

--- linkedListScript.js
+++ linkedListScript.js
@@ -1,11 +1,12 @@
 // modelo para criação de objetos do tipo usuário
 // todas as operações serão feitas a partir da "cabeça" da lista ligada
 class User {
-    constructor(name, cpf, id) {
+    constructor(name, cpf, id, priority) {
         this.name = name;
         this.cpf = cpf;
         this.id = id;
         this.status = 'Ok';
+        this.priority = priority;
         this.next = null;
     };
 };
@@ -17,8 +18,9 @@ class Queue {
         this.end = null;
         this.length = 0;
     }
-    // função para adição de usuários na fila
-    addNextUser(name, cpf){
+    // método para adição de usuários na fila
+    // deve ser utilizado para adicionar um usuário na fila
+    addNextUser(name, cpf, priority){
         const newUser = new User(name, cpf, this.length+1);
         if (this.end != null) {
             this.end.next = newUser;
@@ -29,7 +31,8 @@ class Queue {
         }
         this.length++;
     };  
-    // função para remover o primeiro usuário da fila
+    // método para remover o primeiro usuário da fila
+    // deve ser utilizado quando uma consulta for concluída
     removeTopUser() {
         if (this.start == null){
             return null;
@@ -41,11 +44,12 @@ class Queue {
         }
         this.length--;
     };
-    // função para exibir o usuário no topo da fila
+    // método para exibir o usuário no topo da fila
     showTopUser() {
         return this.start;
     };
-    // função para trocar usuários no caso de espera
+    // método para trocar usuários no caso de espera
+    // deve ser utilizado para trocar a posição de um usuário ausente
     // obs.: quando o usuário é trocado com o último elemento da lista,
     // o campo "end" continua sendo o elemento anterior, porém o campo "next"
     // se torna o usuário a ser esperado.
@@ -78,7 +82,8 @@ class Queue {
             this.end = nextUser;
         }
     };
-    // função de espera
+    // método de espera
+    // deve ser utilizado quando algum usuário não estiver presente e for o próximo na fila
     waitForUser(userId) {
         if (this.start == null){
             return null;
@@ -95,11 +100,11 @@ class Queue {
     };
 };
 // testes com a estrutura de dados criada acima
-/*
 teste = new Queue();
 teste.addNextUser('Camilo', '123');
 teste.addNextUser('de Lellis', '789');
 teste.addNextUser('Medeiros', '789');
+teste.addNextUser('Santos', '321');
+teste.addNextUser('Rosendo', '654');
 teste.waitForUser(2);
-console.log(teste);
-*/
+console.log(teste.start.next);
