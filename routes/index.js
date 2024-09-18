b5f9b5e1372468dc7d6bef5ae4d64790f56d6007
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-09-17 22:26:09 -0300

    adicionada rota para pagina de login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('paginaInicial', { title: 'Fila de atendimento' });
 });
 
+/* GET login page. */
+router.get('/loginpage', (req, res) => {
+  console.log("Acessou a rota /loginpage");
+  res.render('login', { title: 'LOGIN' });
+})
 module.exports = router;
